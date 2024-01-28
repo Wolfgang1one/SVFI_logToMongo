366ec16e2c216c554e3105006703d43deb26d371
Wolfgang1one <891460493@qq.com>
2024-01-28 10:32:30 +0800

    Add sql

---
+++ console.sql
@@ -0,0 +1,17 @@
+//这是创建database的命令，名字可以随便改
+//use LogToMongo;
+
+//这是创建collection的命令，名字可以随意改
+//db.createCollection("test")
+
+//显示所有的database
+//show dbs;
+
+//显示所有的collection
+//show collections;
+
+//查看命令，db后面是collection的名字
+//db.test.find();
+
+//删除命令，后面是where条件
+//db.test.remove({'all_frames_cnt' : 120})
